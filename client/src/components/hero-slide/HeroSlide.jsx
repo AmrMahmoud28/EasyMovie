ddd8fa5f49c56a9f9e96634a15c4bc037ab8bd9b
Amr <dramr2852001@gmail.com>
2023-06-25 00:30:56 +0300

    Edited the transision delay

--- client/src/components/hero-slide/HeroSlide.jsx
+++ client/src/components/hero-slide/HeroSlide.jsx
@@ -10,14 +10,14 @@ const HeroSlide = ({ movie }) => {
   SwiperCore.use([Autoplay]);
   const heroRef = useRef(null);
 
-  const [isActive, setIsActive] = useState(false);
+  // const [isActive, setIsActive] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      // heroRef.current?.classList?.add("active");
-      setIsActive(true);
-    }, 700);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     // heroRef.current?.classList?.add("active");
+  //     // setIsActive(true);
+  //   }, 700);
+  // }, []);
 
   const [modal, setModal] = useState(false);
   const [trailerIndex, setTrailerIndex] = useState(0);
@@ -30,57 +30,57 @@ const HeroSlide = ({ movie }) => {
   return (
     <div className="hero-slide">
       <Swiper
+        speed={1500}
         modules={[Autoplay]}
         grabCursor={true}
         spaceBetween={0}
         slidesPerView={1}
         autoplay={{ delay: 4000 }}
-        onSlideChange={() => console.log('slide change')}
-        // onSwiper={(swiper) => console.log(swiper)}
       >
         {movie.map((item, key) => (
           <SwiperSlide key={key}>
-            <div
-              // ref={heroRef}
-              className={`hero-slide__item ${isActive && "active"}`}
-              style={{
-                backgroundImage: `url(${item.movieBg})`,
-              }}
-            >
-              <div className="hero-slide__item__content container">
-                <div className="hero-slide__item__content__info">
-                  <Link to={`/detail/${item?._id}`}>
-                    <h2 className="title">{item.movieName}</h2>
-                  </Link>
-                  <div className="overview">{item.movieOverview}</div>
-                  <div className="btns">
-                    <Link
-                      to={
-                        item.movieGenre?.split("-")[0] === "Series"
-                          ? `/detail/${item?._id}`
-                          : {
-                              pathname: `/watch/${("" + item.movieName)
-                                .toLowerCase()
-                                .replaceAll(" ", "-")}`,
-                              moviePageUrl: item.movieVideo,
-                              movieBg: item.movieBg,
-                            }
-                      }
-                    >
-                      <Button>Watch now</Button>
+            {({ isActive }) => (
+              <div
+                className={`hero-slide__item ${isActive && "active"}`}
+                style={{
+                  backgroundImage: `url(${item.movieBg})`,
+                }}
+              >
+                <div className="hero-slide__item__content container">
+                  <div className="hero-slide__item__content__info">
+                    <Link to={`/detail/${item?._id}`}>
+                      <h2 className="title">{item.movieName}</h2>
                     </Link>
+                    <div className="overview">{item.movieOverview}</div>
+                    <div className="btns">
+                      <Link
+                        to={
+                          item.movieGenre?.split("-")[0] === "Series"
+                            ? `/detail/${item?._id}`
+                            : {
+                                pathname: `/watch/${("" + item.movieName)
+                                  .toLowerCase()
+                                  .replaceAll(" ", "-")}`,
+                                moviePageUrl: item.movieVideo,
+                                movieBg: item.movieBg,
+                              }
+                        }
+                      >
+                        <Button>Watch now</Button>
+                      </Link>
 
-                    <OutlineButton onClick={(index) => toggleModal(key)}>
-                      Watch trailer
-                    </OutlineButton>
+                      <OutlineButton onClick={(index) => toggleModal(key)}>
+                        Watch trailer
+                      </OutlineButton>
+                    </div>
+                  </div>
+
+                  <div className="hero-slide__item__content__poster">
+                    <img src={item.moviePoster} alt="" />
                   </div>
                 </div>
-
-                <div className="hero-slide__item__content__poster">
-                  <img src={item.moviePoster} alt="" />
-                </div>
               </div>
-            </div>
+            )}
           </SwiperSlide>
         ))}
       </Swiper>
